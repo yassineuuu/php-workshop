83cab44f4fde5d8445d54f354c0fa4ec290d44f4
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-05-31 19:07:05 +0100

    Enoncé4

--- Enoncé4/index.php
+++ Enoncé4/index.php
@@ -75,3 +75,49 @@
 <hr>
 
 <!-- Perform Operation  -->
+<?php
+    // somme function
+    function somme($a, $b){
+        return $a+$b;
+    };
+
+    // factoriel function
+    function factorielle($a){
+        if($a==0) return 1; else return $a*Factoriel($a-1);
+    };
+
+    //number is prime function
+    function nombre_premier($a){
+        $int=$a;
+        function evaluer($int){
+            if ($int == 1) 
+            return 0; 
+              
+            for ($i = 2; $i <= sqrt($int); $i++){ 
+                if ($int % $i == 0) 
+                    return 0; 
+            } 
+            return 1; 
+        
+        };
+        
+        echo "<h3>Is $int prime?</h3><br>";
+        $op = Evaluate($int); 
+        
+        if ($op == 1) 
+            echo "true"; 
+        else
+            echo "false";
+        };
+    
+
+    // Callback function
+    function performOperation($abc, $a=0, $b=0){
+       echo call_user_func($abc, $a, $b);
+    }
+
+    // try and change the first parametre to (factorielle/somme/nombre_premier) and tap a number as a second parametre
+    // if you chosed (somme) make sure to tap the 2 numbers you want to sum =>ex "echo performOperation('somme',9,1)" the output of that should be 10
+    
+    echo performOperation('nombre_premier',9);
+?>
